d163982acac1f1607f73e420fce1a611e6402607
nlyrthiia <nlyrthiia@outlook.com>
2022-06-03 03:26:58 +0800

    feat: complete

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,1 @@
-type MyReturnType<T> = any
+type MyReturnType<T extends (...arg: any) => any> = T extends (...arg: any) => infer R ? R : any;
--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,3 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [U in K]: T[U]
+}
